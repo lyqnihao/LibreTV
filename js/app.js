e40a91b88e43f10ef5412c71d4c67eb6de81914d
lyqnihao <56381217+lyqnihao@users.noreply.github.com>
2025-05-11 04:40:24 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // 全局变量
-let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["tyyszy","dyttzy", "bfzy", "ruyi"]'); // 默认选中天涯资源、暴风资源和如意资源
+let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["tyyszy", "bfzy", "ruyi","zy360","wolong", "hwba","jisu","mdzy","zuid", "dbzy"]'); // 默认选中天涯资源、暴风资源和如意资源，添加360、卧龙、华为吧、极速、魔都、最大和豆瓣资源
 let customAPIs = JSON.parse(localStorage.getItem('customAPIs') || '[]'); // 存储自定义API列表
 
 // 添加当前播放的集数索引
@@ -27,8 +27,8 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // 设置默认API选择（如果是第一次加载）
     if (!localStorage.getItem('hasInitializedDefaults')) {
-        // 仅选择天涯资源、暴风资源和如意资源
-        selectedAPIs = ["tyyszy", "bfzy","dyttzy", "ruyi"];
+        // 默认选中天涯资源、暴风资源和如意资源，添加360、卧龙、华为吧、极速、魔都、最大和豆瓣资源
+        selectedAPIs = ["tyyszy", "bfzy", "ruyi","zy360","wolong", "hwba","jisu","mdzy","zuid", "dbzy"];
         localStorage.setItem('selectedAPIs', JSON.stringify(selectedAPIs));
         
         // 默认选中过滤开关
